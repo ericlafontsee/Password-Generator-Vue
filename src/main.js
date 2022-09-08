b91465debf2c7b55277d5a5b51f66fe350f6b9c9
Eric LaFontsee <ericlafontsee@Erics-MacBook-Pro.local>
2022-09-08 09:36:23 -0500

    reverted back to commit d246652

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import TheHeader from './components/layouts/TheHeader.vue';
-const { createVuetify } = Vuetify
-const vuetify = createVuetify()
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-const app = createApp(App);
-app.component('the-header', TheHeader);
+loadFonts()
 
-app.mount('#app');
-
-
-
-app.use(vuetify).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
