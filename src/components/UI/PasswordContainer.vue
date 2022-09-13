b783253fc2984cbc81e8f1287e69f10f398fdfd8
Eric LaFontsee <ericlafontsee@Erics-MacBook-Pro.local>
2022-09-12 19:30:06 -0500

    added form questions to determine what kind of chars user wants as well as added arrays with those characters

--- src/components/UI/PasswordContainer.vue
+++ src/components/UI/PasswordContainer.vue
@@ -19,6 +19,47 @@
           ref="passwordLengthInput"
         />
       </div>
+      <div class="form-control">
+      <div>Do you want upper case letters?</div>
+      <input
+          id="uppercaseLetters"
+          name="uppercaseLetters"
+          type="checkbox"
+          v-model="userUppercaseInput"
+        />
+      <label for="uppercaseLetters">{{userUppercaseInput}}</label>
+</div>
+<div class="form-control">
+      <div>Do you want lower case letters?</div>
+      <input
+          id="lowercaseLetters"
+          name="lowercaseLetters"
+          type="checkbox"
+          v-model="userLowercaseInput"
+        />
+      <label for="lowercaseLetters">{{userLowercaseInput}}</label>
+</div>
+<div class="form-control">
+      <div>Do you want any special characters?</div>
+      <input
+          id="specialChar"
+          name="specialChar"
+          type="checkbox"
+          v-model="specialCharInput"
+        />
+      <label for="specialChar">{{specialCharInput}}</label>
+</div>
+<div class="form-control">
+      <div>Do you want numbers?</div>
+      <input
+          id="numbers"
+          name="numbers"
+          type="checkbox"
+          v-model="numbersInput"
+        />
+      <label for="numbers">{{numbersInput}}</label>
+</div>
+
       <button type="submit">Generate Password</button>
     </form>
   </div>
@@ -30,24 +71,57 @@ import PasswordOutput from './PasswordOutput.vue';
 export default {
   components: {
     PasswordOutput,
-    SavedPasswords
+    SavedPasswords,
   },
   data() {
     return {
       userPasswordLength: 0,
+      userUppercaseInput: false,
+      userLowercaseInput: false,
+      specialCharInput: false,
+      numbersInput: false,
       password: '',
     };
   },
 
   methods: {
     generatePassword() {
-      let choices =
-        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+      // let choices =
+      //   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+      let upperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let upperAlphaArr = upperAlpha.split('');
+      let lowerAlpha = upperAlpha.toLowerCase();
+      let lowerAlphaArr = lowerAlpha.split('');
+      let num = '0123456789';
+      let numArr = num.split('');
+      let symbols = '!#$%&()*+,-./:;<=>?@^[\\]^_`{|}~';
+      let symbolArr = symbols.split('');
+      let availChars = [];
 
       this.userPasswordLength = this.$refs.passwordLengthInput.value;
+
+
+      if(this.userUppercaseInput){
+        availChars.push.apply(availChars, upperAlphaArr);
+        console.log("uppercase", availChars);
+      }
+      if(this.userLowercaseInput){
+        availChars.push.apply(availChars, lowerAlphaArr);
+        console.log("lowercase", availChars);
+      }
+      if(this.specialCharInput){
+        availChars.push.apply(availChars, symbolArr);
+        console.log("special char", availChars);
+      }
+
+      if(this.numbersInput){
+        availChars.push.apply(availChars, numArr);
+        console.log("numbers", availChars);
+      }
+
       for (var i = 0; i < this.userPasswordLength; i++) {
-        let randomChoice = Math.floor(Math.random() * choices.length);
-        this.password += choices[randomChoice];
+        let randomChoice = Math.floor(Math.random() * availChars.length);
+        this.password += availChars[randomChoice];
       }
       return;
     },
@@ -59,22 +133,24 @@ export default {
         id: new Date().toISOString(),
         password: this.password,
       };
-      fetch('https://password-generator-680b9-default-rtdb.firebaseio.com/passwords.json',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id: newPassword.id,
-          password: newPassword.password
-        })
-      })
+      fetch(
+        'https://password-generator-680b9-default-rtdb.firebaseio.com/passwords.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: newPassword.id,
+            password: newPassword.password,
+          }),
+        }
+      );
     },
     //     removePassword(pwID){
     //    const pwIndex = this.savedPasswords.findIndex(pw => pw.id === pwID);
     //    this.savedPasswords.splice(pwIndex, 1);
     // },
-
   },
 };
 </script>
